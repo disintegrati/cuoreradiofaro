97044f53fc6fe150bd9ecec9bd5d7032c8c7aee9
mediaintegrati <info@mediaintegrati.it>
2018-11-29 15:51:25 +0100

    pronto

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+var sys = require('sys')
+var exec = require('child_process').exec;
+
 const timesync =  require ('timesync');
-var Sound = require('aplay');
 var counter = 0;
 var ts = timesync.create({
     server: 'http://battito.cuoredinapoli.net/timesync',
-    interval: 10000
+    interval: 3000
   });
-  // get notified on changes in the offset
   ts.on('change', function (offset) {
 //    console.log('changed offset: ' + offset + ' ms');
   });
@@ -13,23 +14,28 @@ var ts = timesync.create({
   //get synchronized time
   setInterval(function () {
     var now = new Date(ts.now());
-    
+
     if((now.getSeconds()%3==1 && counter==0) ){
-counter=1;
-	var music = new Sound();
-music.play('./battito.wav');
-music.on('complete', function(){
-counter=0;
-});	
+	counter=1;
 	console.log("batto");
-	
-    }
+	go();
+	}
+
     else{
-	
-       // console.log('non batto');
+      // console.log('non batto');
        //aggiungi qui funzione per stoppare tutto
-	
+
     }
+
   }, 1);
 
-
+	function go(){
+		child = exec("python /home/pi/Desktop/cuoreradiofaro/pulsa.py", function (error, stdout, stderr) {
+ // sys.print('stdout: ' + stdout);
+//  sys.print('stderr: ' + stderr);
+counter = 0;
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+});
+	}
