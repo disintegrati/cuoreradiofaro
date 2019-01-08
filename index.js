e288f813ef1b0b220f31fd27a361a068f0d0029c
mediaintegrati <info@mediaintegrati.it>
2019-01-08 14:17:26 +0000

    versione prod

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var sys = require('sys')
-var exec = require('child_process').exec;
 
+const pigpio = require('pigpio');
+const Gpio = pigpio.Gpio
 const timesync =  require ('timesync');
+const led = new Gpio(17, {mode: Gpio.OUTPUT});
+let dutyCycle = 0;
+let increment = 0;
+let pulsazione = 0;
 var counter = 0;
+var main;
 var ts = timesync.create({
     server: 'http://battito.cuoredinapoli.net/timesync',
     interval: 3000
@@ -11,29 +16,60 @@ var ts = timesync.create({
 //    console.log('changed offset: ' + offset + ' ms');
   });
 
-  //get synchronized time
   setInterval(function () {
     var now = new Date(ts.now());
-
-    if((now.getSeconds()%3==1 && counter==0) ){
-	counter=1;
-	console.log("batto");
-	go();
-	}
-
-    else{
+    if((now.getSeconds()%3==1) && (counter == 0)){
+	counter = 1;
+	increment = 0;
+	dutyCycle = 0;
+	pulsazione = 0;
+	main = setInterval(pulsa, 24);
+	} else{
       // console.log('non batto');
-       //aggiungi qui funzione per stoppare tutto
-
     }
 
-  }, 1);
+ }, 1);
 
-	function go(){
-		child = exec("python /home/pi/Desktop/cuoreradiofaro/pulsa.py", function (error, stdout, stderr) {
-counter = 0;
-  if (error !== null) {
-    console.log('exec error: ' + error);
-  }
-});
-	}
+function pulsa() {
+		led.pwmWrite(dutyCycle);
+		dutyCycle += increment;
+		//console.log(pulsazione);
+		if (pulsazione == 0) {
+		increment = 40;
+		if (dutyCycle >=255){dutyCycle = 255; pulsazione =1;}
+		}
+
+		if (pulsazione == 1) {
+		increment = -20;
+		if (dutyCycle <=0) {dutyCycle = 0; pulsazione = 2;}
+		}
+
+		if (pulsazione == 2) {
+		increment = 44;
+		if (dutyCycle >=255) {dutyCycle = 255; pulsazione = 3;}
+		}
+
+		if (pulsazione == 3) {
+		increment = -6;
+		if (dutyCycle <=5) {dutyCycle = 5; pulsazione = 4;}
+		}
+
+ 		if (pulsazione == 4) {
+		increment = -1;
+		if (dutyCycle <=0) {dutyCycle = 0; pulsazione = 5;}
+		}
+		if (pulsazione == 5) {
+		stopPulsa();
+		}
+	};
+
+function stopPulsa(){
+//	pulsazione = 0;
+	increment = 0;
+	counter = 0;
+	dutyCycle = 0;
+	led.pwmWrite(0);
+	clearInterval(main);
+	main = 0;
+	//console.log('ciao');
+}
